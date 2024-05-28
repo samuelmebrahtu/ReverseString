e63fedc28c4d37cfd1dfa9abc35d341507b1b3b7
samuelmebrahtu <157595431+samuelmebrahtu@users.noreply.github.com>
2024-05-28 14:47:45 +0100

    First successful program

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ReverseString {
     public static void main(String[] args) {
-        System.out.println("Testing 1");
-        System.out.println("Testing 2");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string >> ");
+        String input = scanner.nextLine();
+        scanner.close();
+        String reversedString = reverseString(input);
+        System.out.println("Your reversed string is \""+reversedString+"\"");
+    }
+
+    public static String reverseString(String s) {
+        List<String> reversedStringList = new ArrayList<>();
+        String[] letters = s.split("");
+        for (String c: letters) {
+            reversedStringList.add(0, c);
+        }
+
+        return String.join("", reversedStringList);
     }
 }
