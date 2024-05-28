613f36c59ae3637d05fb8b02e7139f2c7caa5ee2
samuelmebrahtu <157595431+samuelmebrahtu@users.noreply.github.com>
2024-05-28 13:51:52 +0100

    Just two test string outputs

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,5 +1,6 @@
 public class ReverseString {
     public static void main(String[] args) {
-        System.out.println("Testing");
+        System.out.println("Testing 1");
+        System.out.println("Testing 2");
     }
 }
